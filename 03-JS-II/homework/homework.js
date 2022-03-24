83efaeb8556c9c83be11cb4ac0e9716a543216c2
salasar78 <salasar78@hotmail.com>
2022-03-24 17:07:14 -0500

    homework JSI

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,10 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+}
+return y;
 }
 
 function mayoriaDeEdad(edad) {
